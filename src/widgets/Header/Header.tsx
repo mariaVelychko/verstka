5296d090fea6c48810c19713319f138db246b7c5
MariaVelychkoPolytech <197424840+mariaVelychko@users.noreply.github.com>
2025-09-19 22:15:27 +0300

    Update header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -2,34 +2,37 @@ import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.header__logo}>
-        <h5 className={styles.header__logo_name}>Medifit</h5>
-      </div>
-      <nav className={styles.header__nav}>
-        <a href="/" className={styles.header__nav_item}>
-          Home
-        </a>
-        <a href="/" className={styles.header__nav_item}>
-          Shop
-        </a>
-        <a href="/" className={styles.header__nav_item}>
-          Pages
-        </a>
-        <a href="/" className={styles.header__nav_item}>
-          About
-        </a>
-        <a href="/" className={styles.header__nav_item}>
-          Contact
-        </a>
-      </nav>
-      <div className={styles.header__actions}>
-        <button className={styles.header__search}>
-          <img src="/icons/search.svg" alt="Search" />
-        </button>
-        <a href="/" className={styles.header__cart}>
-          <img src="/icons/cart.svg" alt="Cart" />
-        </a>
+    <header className={styles.header_wrapper}>
+      <div className={styles.header_container}>
+        <div className={styles.header__logo}>
+          <img src="./public/logo/logo.svg" alt="logo Medifit" />
+          <h5 className={styles.header__logo_name}>Medifit</h5>
+        </div>
+        <nav className={styles.header__nav}>
+          <a href="/" className={styles.header__nav_item}>
+            Home
+          </a>
+          <a href="/" className={styles.header__nav_item}>
+            Shop
+          </a>
+          <a href="/" className={styles.header__nav_item}>
+            Pages
+          </a>
+          <a href="/" className={styles.header__nav_item}>
+            About
+          </a>
+          <a href="/" className={styles.header__nav_item}>
+            Contact
+          </a>
+        </nav>
+        <div className={styles.header__actions}>
+          <button className={styles.header__search}>
+            <img src="/icons/search.svg" alt="Search" />
+          </button>
+          <a href="/" className={styles.header__cart}>
+            <img src="/icons/cart.svg" alt="Cart" />
+          </a>
+        </div>
       </div>
     </header>
   );
